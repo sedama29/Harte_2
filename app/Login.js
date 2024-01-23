9e4034c519c1225ab6ad4777e6f5623857a2ae69
A.v.s.Pavan <avspavan1234@gmail.com>
2024-01-22 18:47:05 -0600

    ios started

--- app/Login.js
+++ app/Login.js
@@ -5,13 +5,14 @@ import { WEB_CLIENT_ID } from '../util/key';
 GoogleSignin.configure({
   androidClientId: '31462645493-mn84svkckvu16koamr4aaq64lq5loj6i.apps.googleusercontent.com',
   webClientId: '31462645493-37t5n59nrg8e14esnmjfvka0608ehfse.apps.googleusercontent.com',
+  iosClientId: '31462645493-c786nkss2dsbd55oq8u8lkvsldhh0a6i.apps.googleusercontent.com',
 });
 
 export const signInWithGoogle = async () => {
   try {
 
-    await GoogleSignin.hasPlayServices();
-    const userInfo = await GoogleSignin.signIn();
+    // await GoogleSignin.hasPlayServices();
+    const userInfo = await GoogleSignin[Platform.OS === 'android' ? 'signIn' : 'signInSilently']();
     console.log('User Info:', userInfo);
     await GoogleSignin.revokeAccess();
   } catch (error) {
